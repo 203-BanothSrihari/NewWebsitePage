44f8548a179fe9a6dc3ce0d01ed9f36c59e73bf1
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-15 17:18:32 +0530

    Add project files.

---
+++ NewWebsitePage/Data/NewWebsitePageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NewWebsitePage.Models;
+
+namespace NewWebsitePage.Data
+{
+    public class NewWebsitePageContext : DbContext
+    {
+        public NewWebsitePageContext (DbContextOptions<NewWebsitePageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NewWebsitePage.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ NewWebsitePage/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewWebsitePage.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string MailID { get; set; }
+
+        public int  PhoneNumber { get; set; }
+
+        public string Location { get; set; }
+
+    }
+}
